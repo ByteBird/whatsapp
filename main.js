1c96dbe929eebdb9130f2540982d5566427e35fc
Jhonatan <jhonatan@Jhonatans-iMac.local>
2022-03-24 12:13:29 -0500

    wsp-2

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function eventon(){
-    alert('kge de la risha')
-}
+let text_bar = document.getElementById('main-message')
+let message_box = document.getElementById('mid-container')
+text_bar.addEventListener('keydown', e => {
+    if(e.key === 'Enter'){
+        let message_container = document.createElement('div')
+        let new_text = document.createElement('p')
+        message_container.appendChild(new_text)
+        new_text.classList.add('message')
+        new_text.textContent = text_bar.value
+        message_box.appendChild(message_container)
+        text_bar.value = ''
+    }
+})
